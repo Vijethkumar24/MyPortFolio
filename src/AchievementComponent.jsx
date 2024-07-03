799911b01bb80194cb3d81e17d40adf4867917dc
vin1112 <vkvijethkumar24@gmail.com>
2024-07-03 11:39:41 +0530

    fixedOverflowissues

--- src/AchievementComponent.jsx
+++ src/AchievementComponent.jsx
@@ -1,59 +1,76 @@
-import React, { useState,useRef} from 'react';
-import './Achievement.css';
-import TrophyImg from './assets/imgs/trophy.png'; // Adjust the path as needed
-import PinImg from './assets/imgs/pin.png'; // Adjust the path as needed
+import React, { useState, useRef } from "react";
+import "./Achievement.css";
+import TrophyImg from "./assets/imgs/trophy.png"; // Adjust the path as needed
+import PinImg from "./assets/imgs/pin.png"; // Adjust the path as needed
 
 // Import all achievement images
-import TechProImg from './assets/imgs/tech.png';
-import HacktoberfestImg from './assets/imgs/hacker.png';
-import IdeathonImg from './assets/imgs/idea.png';
-import ShristiImg from './assets/imgs/enterpreneur.png';
-import AKAARImg from './assets/imgs/gaming.png';
-import LeaderImg from './assets/imgs/Leader.png';
+import TechProImg from "./assets/imgs/tech.png";
+import HacktoberfestImg from "./assets/imgs/hacker.png";
+import IdeathonImg from "./assets/imgs/idea.png";
+import ShristiImg from "./assets/imgs/enterpreneur.png";
+import AKAARImg from "./assets/imgs/gaming.png";
+import LeaderImg from "./assets/imgs/Leader.png";
 
 function AchievementsComponent() {
   const achievements = [
     {
       title: 'Winners of "Tech-Pro: Best College Project"',
-      description: 'Developed an innovative platform to showcase and recognize exceptional college projects, leading to increased visibility and engagement for student work.',
-      imgsrc: TechProImg
+      description:
+        "Developed an innovative platform to showcase and recognize exceptional college projects, leading to increased visibility and engagement for student work.",
+      imgsrc: TechProImg,
     },
     {
-      title: 'Excitedly participated in Hacktoberfest',
-      description: 'This involves contributing to open-source projects and collaborating with developers.',
-      imgsrc: HacktoberfestImg
+      title: "Excitedly participated in Hacktoberfest",
+      description:
+        "This involves contributing to open-source projects and collaborating with developers.",
+      imgsrc: HacktoberfestImg,
     },
     {
       title: 'Passionately engaged in "Ideathon" an state-level competition',
-      description: 'This involves showcasing my creativity and problem-solving skills to tackle real-world challenges.',
-      imgsrc: IdeathonImg
+      description:
+        "This involves showcasing my creativity and problem-solving skills to tackle real-world challenges.",
+      imgsrc: IdeathonImg,
     },
     {
-      title: 'Confidently contributed to the Idea Business Pitch event "Shristi"',
-      description: 'This event was conducted by VTU, presenting my entrepreneurial vision and passion for innovation.',
-      imgsrc: ShristiImg
+      title:
+        'Confidently contributed to the Idea Business Pitch event "Shristi"',
+      description:
+        "This event was conducted by VTU, presenting my entrepreneurial vision and passion for innovation.",
+      imgsrc: ShristiImg,
     },
     {
       title: 'Gaming lead for AJIET college fest "AKAAR"',
-      description: 'This involved planning and supervising a range of gaming competitions and events to make sure that attendees had a fun and exciting time.',
-      imgsrc: LeaderImg
+      description:
+        "This involved planning and supervising a range of gaming competitions and events to make sure that attendees had a fun and exciting time.",
+      imgsrc: LeaderImg,
     },
     {
-        title: 'Event coordinator of BGMI and Valorant for college fest',
-        description: 'This involved planning and supervising a range of gaming competitions and events to make sure that attendees had a fun.',
-        imgsrc: AKAARImg
-      }
+      title: "Event coordinator of BGMI and Valorant for college fest",
+      description:
+        "This involved planning and supervising a range of gaming competitions and events to make sure that attendees had a fun.",
+      imgsrc: AKAARImg,
+    },
   ];
 
-  const [trophyPosition, setTrophyPosition] = useState({ top: '0%', left: '0%' });
-  const [pinPosition, setPinPosition] = useState({ top: '0%', left: '0%' });
+  const [trophyPosition, setTrophyPosition] = useState({
+    top: "0%",
+    left: "0%",
+  });
+  const [pinPosition, setPinPosition] = useState({ top: "0%", left: "0%" });
   const trophyImageRef = useRef(null);
 
   const handleAchievementHover = (event) => {
-    trophyImageRef.current.style.display = 'block';
-    const { top, left, width, height } = event.currentTarget.getBoundingClientRect();
-    setTrophyPosition({ top: `${top + window.scrollY + height / 2 + 5}px`, left: `${left + window.scrollX + width - 40}px` });
-    setPinPosition({ top: `${top + window.scrollY + height / 2}px`, left: `${left + window.scrollX - 10}px` });
+    trophyImageRef.current.style.display = "block";
+    const { top, left, width, height } =
+      event.currentTarget.getBoundingClientRect();
+    setTrophyPosition({
+      top: `${top + window.scrollY + height / 2 + 5}px`,
+      left: `${left + window.scrollX + width - 60}px`,
+    });
+    setPinPosition({
+      top: `${top + window.scrollY + height / 2}px`,
+      left: `${left + window.scrollX - 10}px`,
+    });
   };
 
   return (
@@ -62,16 +79,21 @@ function AchievementsComponent() {
         className="trophy"
         style={{ top: trophyPosition.top, left: trophyPosition.left }}
       >
-        <img ref={trophyImageRef} src={TrophyImg} alt="trophy" className='trophy'  style={{
-                    display: 'none',
-                    position: 'absolute',
-                }} />
+        <img
+          ref={trophyImageRef}
+          src={TrophyImg}
+          alt="trophy"
+          className="trophy"
+          style={{
+            display: "none",
+            position: "absolute",
+          }}
+        />
       </div>
       <div
         className="pin"
         style={{ top: pinPosition.top, left: pinPosition.left }}
-      >
-      </div>
+      ></div>
       {achievements.map((achievement, index) => (
         <div
           key={index}
@@ -79,7 +101,11 @@ function AchievementsComponent() {
           onMouseEnter={handleAchievementHover}
         >
           <img src={PinImg} alt="Pin" className="pin" />
-          <img src={achievement.imgsrc} alt={achievement.title} className="achievement-image" />
+          <img
+            src={achievement.imgsrc}
+            alt={achievement.title}
+            className="achievement-image"
+          />
           <h3>{achievement.title}</h3>
           <p>{achievement.description}</p>
         </div>
--- src/ProjectsComponent.jsx
+++ src/ProjectsComponent.jsx
@@ -1,56 +1,71 @@
-import React, { useState } from 'react';
-import './Project.css';
-import AstronautImg from './assets/imgs/AstronautChotu.png';
-import Pin from './assets/imgs/pin.png' // Adjust the path as needed
-import Docimg from './assets/imgs/Document.png'
-import Solarimg from './assets/imgs/solar.png'
-import Scannerrimg from './assets/imgs/scanner.png'
-import Gasimg from './assets/imgs/gas.png'
-import Panchimg from './assets/imgs/Panchayath.png'
-import PortFolioimg from './assets/imgs/PortFolio.png'
- 
+import React, { useState } from "react";
+import "./Project.css";
+import AstronautImg from "./assets/imgs/AstronautChotu.png";
+import Pin from "./assets/imgs/pin.png"; // Adjust the path as needed
+import Docimg from "./assets/imgs/Document.png";
+import Solarimg from "./assets/imgs/solar.png";
+import Scannerrimg from "./assets/imgs/scanner.png";
+import Gasimg from "./assets/imgs/gas.png";
+import Panchimg from "./assets/imgs/Panchayath.png";
+import PortFolioimg from "./assets/imgs/PortFolio.png";
 
 function ProjectsComponent() {
   const projects = [
     {
-      title: 'Counterfeit Detection Of Documents Using BlockChain',
-      description: 'Developed a decentralized, immutable blockchain-based system to detect fake documents using Ethereum smart contracts, ensuring the authenticity & integrity.',
-      imgsrc :Docimg
+      title: "Counterfeit Detection Of Documents Using BlockChain",
+      description:
+        "Developed a decentralized, immutable blockchain-based system to detect fake documents using Ethereum smart contracts, ensuring the authenticity & integrity.",
+      imgsrc: Docimg,
     },
     {
-      title: 'TextScanner App',
-      description: 'This App allows users to easily extract text from a variety of photos and PDFs, allowing them to access the content whenever it\'s convenient for them.',
-      imgsrc :Scannerrimg
+      title: "TextScanner App",
+      description:
+        "This App allows users to easily extract text from a variety of photos and PDFs, allowing them to access the content whenever it's convenient for them.",
+      imgsrc: Scannerrimg,
     },
     {
-      title: 'Solar Edge Ecommerce-App',
-      description: 'The App provides a simplified online shopping experience for solar items, allowing users to browse and buy solar panels, inverters, and related equipment in one convenient location.',
-      imgsrc :Solarimg 
+      title: "Solar Edge Ecommerce-App",
+      description:
+        "The App provides a simplified online shopping experience for solar items, allowing users to browse and buy solar panels, inverters, and related equipment in one convenient location.",
+      imgsrc: Solarimg,
     },
     {
-      title: 'HarGhar Gas',
-      description: 'Through the online platform HarGhar Gas, customers can easily reserve LPG gas cylinders from leading providers like Indane, Bharat Gas, and HP Gas.',
-      imgsrc :Gasimg
+      title: "HarGhar Gas",
+      description:
+        "Through the online platform HarGhar Gas, customers can easily reserve LPG gas cylinders from leading providers like Indane, Bharat Gas, and HP Gas.",
+      imgsrc: Gasimg,
     },
     {
-      title: 'Apna Panchayath',
-      description: 'Here people can actively participate by posting their issues and peruse succinct summaries of local panchayats.',
-      imgsrc :Panchimg
+      title: "Apna Panchayath",
+      description:
+        "Here people can actively participate by posting their issues and peruse succinct summaries of local panchayats.",
+      imgsrc: Panchimg,
     },
     {
-      title: 'Portfolio Website',
-      description: 'This is my website, which provides visitors with a glimpse into my professional identity. Designed to showcase my talents, experience, and adventure.',
-      imgsrc :PortFolioimg
-    }
+      title: "Portfolio Website",
+      description:
+        "This is my website, which provides visitors with a glimpse into my professional identity. Designed to showcase my talents, experience, and adventure.",
+      imgsrc: PortFolioimg,
+    },
   ];
 
-  const [astronautPosition, setastronautPosition] = useState({ top: '0%', left: '0%' });
-  const [pinPosition, setPinPosition] = useState({ top: '0%', left: '0%' });
+  const [astronautPosition, setastronautPosition] = useState({
+    top: "0%",
+    left: "0%",
+  });
+  const [pinPosition, setPinPosition] = useState({ top: "0%", left: "0%" });
 
   const handleProjectHover = (event) => {
-    const { top, left, width, height } = event.currentTarget.getBoundingClientRect();
-    setastronautPosition({ top: `${top + window.scrollY + height /2+40}px`, left: `${left + window.scrollX + width-40}px` });
-    setPinPosition({ top: `${top + window.scrollY + height / 2}px`, left: `${left + window.scrollX - 10}px` });
+    const { top, left, width, height } =
+      event.currentTarget.getBoundingClientRect();
+    setastronautPosition({
+      top: `${top + window.scrollY + height / 2 + 5}px`,
+      left: `${left + window.scrollX + width - 120}px`,
+    });
+    setPinPosition({
+      top: `${top + window.scrollY + height / 2}px`,
+      left: `${left + window.scrollX - 10}px`,
+    });
   };
 
   return (
@@ -59,7 +74,7 @@ function ProjectsComponent() {
         className="astronaut"
         style={{ top: astronautPosition.top, left: astronautPosition.left }}
       >
-        <img src={AstronautImg} alt="Rocket" />
+        <img src={AstronautImg} alt="Rocket" className="astronaut_chotu" />
       </div>
       {projects.map((project, index) => (
         <div
@@ -67,11 +82,14 @@ function ProjectsComponent() {
           className="project-card"
           onMouseEnter={handleProjectHover}
         >
-            <img src={Pin} alt="Pin" className="pin" />
-            <img src={project.imgsrc} alt={project.title} className="project-image" />
+          <img src={Pin} alt="Pin" className="pin" />
+          <img
+            src={project.imgsrc}
+            alt={project.title}
+            className="project-image"
+          />
           <h3>{project.title}</h3>
           <p>{project.description}</p>
-         
         </div>
       ))}
     </div>
